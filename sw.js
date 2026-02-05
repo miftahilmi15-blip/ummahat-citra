4e38d1b16b71ca5fa34b365e0b833539364bc7aa
miftahilmi15-blip <miftahilmi15@gmail.com>
2026-02-05 18:41:12 +0700

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
-const CACHE_NAME = 'masterpro-v1';
-const assets = ['index.html'];
+const CACHE_NAME = 'masterpro-v2';
+const assets = [
+  'index.html',
+  'kamalul.png',
+  'manifest.json'
+];
 
+// Proses Install
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('Sistem MasterPro: Mengamankan Aset...');
       return cache.addAll(assets);
     })
   );
 });
 
+// Aktivasi & Hapus Cache Lama
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Ambil Data
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
